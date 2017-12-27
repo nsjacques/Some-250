083a428254632f356f498c1facd4aba9e61c21d1
Noah Jacques <noah.jacques96@gmail.com>
2017-12-27 18:53:19 -0500

    cleaned it up; takes argument now

--- src/Rep250.java
+++ src/Rep250.java
@@ -1,17 +1,18 @@
 /*
 Author: Noah Jacques
 
+Simple natural number base10 to binary converter
+
 */
 public class Rep250 {
 
 	public static void main(String[] args) {
-		System.out.println(rep(637));
+		System.out.println(rep(Integer.parseInt(args[0])));
 	}
 	
 	private static String rep (int n){
 		if(n==0) return "0";
 		if (n==1) return "1";
-		System.out.println("\nn/2 = " + n/2);
 		String w = rep (n/2);
 		if (n%2 == 0)
 			return w + 0;
