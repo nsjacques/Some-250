aebbfb0bdd7a444de53b35096b8e253b3dff6182
Noah Jacques <noah.jacques96@gmail.com>
2017-12-28 00:41:05 -0500

    neatened

--- src/Sieve.java
+++ src/Sieve.java
@@ -3,27 +3,33 @@ Author: Noah Jacques
 
 Sieve of Eratosthenes
 
+Pass it an upper bound; returns string of primes
+
 */
 public class Sieve {
 
 	public static void main(String[] args) {
 
-	int upperBound = Integer.parseInt(args[0]);
-    int upperBoundSquareRoot = (int) Math.sqrt(upperBound);
-    boolean[] isComposite = new boolean[upperBound + 1];//why plus 1?
+		int upperBound = Integer.parseInt(args[0]);
+	    int upperBoundSquareRoot = (int) Math.sqrt(upperBound);
+	    boolean[] isComposite = new boolean[upperBound + 1];//why plus 1?
 
-    for (int m = 2; m <= upperBoundSquareRoot; m++) {
-        if (!isComposite[m]) {
-            System.out.print(m + " ");
+	    String primes = "";
 
-                for (int k = m * m; k <= upperBound; k += m)
-                    isComposite[k] = true;
-	            }
-
+	    for (int m = 2; m <= upperBoundSquareRoot; m++) {
+	        if (!isComposite[m]) {
+	            primes+= m + " ";
+	            for (int k = m * m; k <= upperBound; k += m)
+	                isComposite[k] = true;
+	        }
 	    }
-	    for (int m = upperBoundSquareRoot; m <= upperBound; m++)
-        	if (!isComposite[m])
-                System.out.print(m + " ");
+
+	    for (int m = upperBoundSquareRoot; m <= upperBound; m++){
+	    	if (!isComposite[m])
+	            primes += m + " ";
 		}
 
+		System.out.printf("\nThere are %d primes from 2 to %s:\n", primes.split(" ").length, args[0]);
+		System.out.println(primes);
+	}
 }
