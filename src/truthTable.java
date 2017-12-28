0f4e92157f4f6146af2166a06a5f559cd4419043
Noah Jacques <noah.jacques96@gmail.com>
2017-12-28 00:48:17 -0500

    neatened

--- src/truthTable.java
+++ src/truthTable.java
@@ -5,23 +5,26 @@ Author: Noah Jacques
 public class truthTable {
 
 	public static void main(String[] args) {
-		printTruthTable(4);
+		printTruthTable(Integer.parseInt(args[0]));
 	}
 	
 	//k is number of variables
 	public static void printTruthTable(int k) {
-		//sets numberLines to 2^k
-		int numberLines = 1;
-	    for (int i = 0; i < k; i++) {
-	        numberLines *= 2;
-	    }
-	    //calls makeline numberLines times with k and lineNumber for LN 0 to numberLines
+
+		int numberLines = (int) Math.pow(2,k);
+
+		String output = "";
+
+	    //calls makeline 2^k times with k and lineNumber for LN 0 to numberLines
 	    for (int lineNumber = 0; lineNumber < numberLines; lineNumber++) {
-	        System.out.println(lineNumber + " in binary: " + makeLine(k, lineNumber));
+	        output+= makeLine(k, lineNumber) + "\n";
 	    }
+
+	    System.out.println(output);
 	}
 	
 	public static String makeLine(int k, int lineNumber) {
+
 		//w set to LN % 2 (0 or 1)
 	    String w = String.valueOf(lineNumber % 2);
 	    //for i from 1 to k
